671ebd5224cbfc7f061ec428beb1ea915f8260d7
nitishsharma2825 <sharnit2825@gmail.com>
2025-08-12 00:48:11 +0530

    added global variables, print statements

--- vm.c
+++ vm.c
@@ -34,11 +34,13 @@ void initVM()
 {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM()
 {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -83,6 +85,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
         if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) \
@@ -122,6 +125,38 @@ static InterpretResult run()
             case OP_NIL: push(NIL_VAL); break;
             case OP_TRUE: push(BOOL_VAL(true)); break;
             case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_POP: pop(); break;
+            case OP_GET_GLOBAL:
+            {
+                ObjString* name = READ_STRING();
+                Value value;
+                if (!tableGet(&vm.globals, name, &value))
+                {
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(value);
+                break;
+            }
+            case OP_DEFINE_GLOBAL:
+            {
+                ObjString* name = READ_STRING();
+                tableSet(&vm.globals, name, peek(0));
+                pop();
+                break;
+            }
+            case OP_SET_GLOBAL:
+            {
+                ObjString* name = READ_STRING();
+                if (tableSet(&vm.globals, name, peek(0)))
+                {
+                    // can only set existing variables
+                    tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
             case OP_EQUAL:
             {
                 Value b = pop();
@@ -188,10 +223,14 @@ static InterpretResult run()
                 push(BOOL_VAL(isFalsey(pop())));
                 break;
             }
-            case OP_RETURN:
+            case OP_PRINT:
             {
                 printValue(pop());
                 printf("\n");
+                break;
+            }
+            case OP_RETURN:
+            {
                 return INTERPRET_OK;
             }
         }
@@ -199,6 +238,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
--- vm.h
+++ vm.h
@@ -13,6 +13,7 @@ typedef struct
     uint8_t* ip; // Instruction pointer
     Value stack[STACK_MAX];
     Value* stackTop;
+    Table globals;
     Table strings; // string interning, used as set to keep only unique strings
     Obj* objects; // pointer to head of list of objects to free
 } VM;
