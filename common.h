495db6b4679f3070b9b32446845cf53786fae272
nitishsharma2825 <sharnit2825@gmail.com>
2025-10-11 12:56:18 +0530

    optimize 2: NaN boxing to reduce space

--- common.h
+++ common.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define NAN_BOXING
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
@@ -19,5 +20,6 @@
 #undef DEBUG_TRACE_EXECUTION
 #undef DEBUG_STRESS_GC
 #undef DEBUG_LOG_GC
+// #define NAN_BOXING
 
 #endif
